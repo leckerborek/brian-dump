b77a1dda0106ed5dba504216e6445f0764ca3b66
jlorek <lorek.jens@gmail.com>
2020-01-27 22:09:48 +0100

    crawling: check!

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,20 +1,24 @@
 import { Controller, Get, Req, Res, Post, Body } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { CreateItemDto } from './item';
+import { ItemsService } from 'src/items.service';
+import { WebContent } from './webContent';
 
 @Controller('items')
 export class ItemsController {
+    constructor(private readonly itemsService: ItemsService) {}
+
     @Get()
-    findAll() : string  {
-    //findAll(@Req() req: Request, @Res() res: Response) {
+    findAll(): string {
+        //findAll(@Req() req: Request, @Res() res: Response) {
         //console.log(req.url);
-        return 'this will return everything!'
+        return 'this will return everything!';
     }
 
     @Post()
-    create(@Body() item: CreateItemDto) : CreateItemDto {
+    async create(@Body() item: CreateItemDto): Promise<WebContent> {
         console.table(item);
-        item.id = "12345";
-        return item;
+        const result = await this.itemsService.extract(item.url);
+        return result;
     }
 }
