9e5c23fbb85a0f7248900886f6f0bf0cfd7b585c
Jens Becker <jens.je.becker@deutschebahn.com>
2020-02-08 20:36:31 +0100

    add /api global prefix

--- src/service/src/main.ts
+++ src/service/src/main.ts
@@ -1,19 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix("/api");
 
-    const options = new DocumentBuilder()
-        .setTitle('Brian Dump')
-        .setDescription('The Brian Dump API description.')
-        .setVersion('0.1')
-        .build();
+  const options = new DocumentBuilder()
+    .setTitle("Brian Dump")
+    .setDescription("The Brian Dump API description.")
+    .setVersion("0.1")
+    .build();
 
-    const document = SwaggerModule.createDocument(app, options);
-    SwaggerModule.setup('swagger', app, document);
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup("swagger", app, document);
 
-    await app.listen(3000);
+  await app.listen(3000);
 }
 bootstrap();
